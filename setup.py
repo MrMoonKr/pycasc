d350447b2fdd19413d99da9fe6920f9617265322
Meithal <hurin8888@gmail.com>
2023-04-26 16:17:18 +0200

    Better system architecture detection

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ import subprocess
 import pathlib
 import shutil
 import configparser
+import platform
 
 mode = 'Release'  # Debug/Release
-plat = 32  if sys.platform == 'win32' else 64  # 32 or 64
+plat = 32 if platform.architecture()[0] == '32bit' else 64  # 32 or 64
 os.environ['DISTUTILS_DEBUG'] = "1" if mode == 'Debug' else ""
 
 from setuptools import Extension, setup
@@ -14,19 +15,19 @@ from distutils.util import get_platform
 from distutils.ccompiler import new_compiler
 
 extensions = []
-print("setup.py mode", sys.argv[1])
+print("setup.py mode", sys.argv[1] if len(sys.argv) > 1 else "none")
 print("platform", sys.platform)
 
 if not sys.argv[1] == 'install':
     if not (pathlib.Path(__file__) / '..' / 'CascLib').is_dir():
         print("didn't find CascLib, cloning from repo...")
-        subprocess.run(["git", "clone", "https://github.com/ladislav-zezula/CascLib.git"])
+        subprocess.run(["git", "clone", "https://github.com/ladislav-zezula/CascLib.git"], shell=True)
 
     bname32 = 'build86'
     bname64 = 'build64'
-    build64_dir = pathlib.Path(__file__) / '..' / 'CascLib' / bname64
-    build32_dir = pathlib.Path(__file__) / '..' / 'CascLib' / bname32
-    buil_dir = {32: build32_dir, 64: build64_dir}.get(plat, build64_dir)
+    buil_dir = {
+        32: pathlib.Path(__file__) / '..' / 'CascLib' / bname32,
+        64: pathlib.Path(__file__) / '..' / 'CascLib' / bname64}.get(plat)
     cmake_opts = {32: ["-A", "Win32"], 64: ["-A", "x64"]}.get(plat, [])
     setup_build_plat = {32: 'win32', 64: 'win-amd64'}.get(plat, get_platform())
     bname = {32: bname32, 64: bname64}.get(plat, bname64)
@@ -38,7 +39,7 @@ if not sys.argv[1] == 'install':
 
     setup_cfg = configparser.ConfigParser()
     setup_cfg['build'] = {
-        'plat-name': setup_build_plat,
+        'plat_name': setup_build_plat,
         'debug': True if mode == 'Debug' else False
     }
     with (pathlib.Path(__file__) / '..' / 'setup.cfg').open('w') as cfg_file:
@@ -79,8 +80,7 @@ if not sys.argv[1] == 'install':
             include_dirs=[
                 str(include_dir)
             ] ,
-            library_dirs=[str((buil_dir / mode).resolve())]              
-                ,
+            library_dirs=[str((buil_dir / mode).resolve())],
             define_macros=[('CASCLIB_NO_AUTO_LINK_LIBRARY', None), ('_WIN32', '1') if plat == 32 else ('_WIN64', '1')],
             libraries=['casc'],
         )
